dca16f8ba72db5e1c5fe9830a942350dcd9b4c0b
Tomasz Malisiewicz <tom@vision.ai>
2015-01-09 18:33:48 -0500

    add version file

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ sed 's/Docs9/Docs '`git describe --tags`'/g' mkdocs.yml > mkdocs2.yml
 cp mkdocs.yml mkdocs.yml.save
 cp mkdocs2.yml mkdocs.yml
 mkdocs build -use_directory_urls=false 
+echo VMXdocs_`git describe --tags` > site/version
 mv mkdocs.yml.save mkdocs.yml
 rm mkdocs2.yml
 cd mcr
