a825ec6988a9c460fd899528f0e57deda010aa87
Tomasz Malisiewicz <tom@vision.ai>
2015-01-09 16:58:41 -0500

    clean things up

--- compile_local.sh
+++ compile_local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp custom_theme/base.html custom_theme/base.html.save
+cp custom_theme/base.html base.html.save
 cp custom_theme/base2.html custom_theme/base.html
 
 ./compile.sh
-mv custom_theme/base.html.save custom_theme/base.html
+mv base.html.save custom_theme/base.html
