271ba0eb7a0f3262e81ca833da9afbb3b6a4bbb7
Tomasz Malisiewicz <tom@vision.ai>
2015-01-09 18:22:14 -0500

    remove dirty tag

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 rm -rf site/
 mkdir site
-sed 's/Docs9/Docs '`git describe --tags --dirty`'/g' mkdocs.yml > mkdocs2.yml
+sed 's/Docs9/Docs '`git describe --tags`'/g' mkdocs.yml > mkdocs2.yml
 cp mkdocs.yml mkdocs.yml.save
 cp mkdocs2.yml mkdocs.yml
 mkdocs build -use_directory_urls=false 
