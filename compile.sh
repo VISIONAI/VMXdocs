8bc9bfe8c7c39ca4bcd90e638b925e3710069f0f
Tomasz Malisiewicz <tom@vision.ai>
2015-01-09 15:21:10 -0500

    modify compilation with version string

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
 rm -rf site/
 mkdir site
+sed 's/Version:/Version:'`git describe --tags --dirty`'/g' mkdocs.yml > mkdocs2.yml
+cp mkdocs.yml mkdocs.yml.save
+cp mkdocs2.yml mkdocs.yml
 mkdocs build -use_directory_urls=false 
+cp mkdocs.yml.save mkdocs.yml
 cd mcr
 ./generate_docs.sh
 cp -R docs/* ../site/
