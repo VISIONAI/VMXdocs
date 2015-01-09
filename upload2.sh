63ddb67e7accacd84aa797990ece97d535005506
Tomasz Malisiewicz <tom@vision.ai>
2015-01-09 17:04:21 -0500

    updates

--- upload2.sh
+++ upload2.sh
@@ -4,7 +4,7 @@ rm -rf tmp/docs
 cp -R site/ tmp/docs/
 F=VMXdocs_`git describe --tags --dirty`.tar
 cd tmp/
-rm ../$F.gz
+rm ../$F.gz > /dev/null 2>&1
 tar cf ../$F docs/
 rm -rf docs/
 cd -
@@ -13,8 +13,8 @@ gzip $F
 
 if [ "`ls $F.gz | grep dirty`" == "" ];
 then
-
     echo 'not dirty'
+    scp $F.gz root@files.vision.ai:/www/vmx/docs/
 else
     echo 'dirty'
 fi
