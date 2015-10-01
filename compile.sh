1db3bd57a855138055f195ce18b539608c29b742
Tomasz Malisiewicz <tom@vision.ai>
2015-10-01 18:54:25 -0400

    blow out old API docs

--- compile.sh
+++ compile.sh
@@ -7,6 +7,8 @@ mkdocs build -use_directory_urls=false
 echo VMXdocs_`git describe --tags` > site/version
 mv mkdocs.yml.save mkdocs.yml
 rm mkdocs2.yml
+rm -rf ./site/VMXapi
+cp -R api ./site/VMXapi
 cd mcr
 ./generate_docs.sh
 cp -R docs/* ../site/
